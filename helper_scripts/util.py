44d62de3ff9166e34c3493e8a7a50c64206166fe
Vincent <vjc9@duke.edu>
2024-05-06 01:48:55 -0400

    fixed missing folder error

--- helper_scripts/util.py
+++ helper_scripts/util.py
@@ -2,6 +2,10 @@ import os
 import shutil
 
 def delete_old_files(directory, num_to_keep=5):
+
+    if not os.path.exists(directory):
+        return
+
     try:
         # Get a list of all files in the directory along with their modification times
         files = [(os.path.join(directory, f), os.path.getmtime(os.path.join(directory, f))) for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
@@ -18,6 +22,10 @@ def delete_old_files(directory, num_to_keep=5):
         print(f"Failed to delete files: {e}")  # circular import prevents the use of Logger here
 
 def delete_old_folders(directory, num_to_keep=5):
+    
+    if not os.path.exists(directory):
+        return
+
     try:
         # Get a list of all directories in the specified directory
         directories = [(os.path.join(directory, d), os.path.getmtime(os.path.join(directory, d))) for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
